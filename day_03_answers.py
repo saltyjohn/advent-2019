ff5b32c8a3ee769c2dd1968eb5b28a9bdcb26607
John Salzman <john.r.salzman@gmail.com>
2019-12-03 14:54:40 -0500

    got tests to pass, but I have too much looping and running this takes forever

--- day_03_answers.py
+++ day_03_answers.py
@@ -2,6 +2,7 @@ from day_03_inputs import wires, traverse_tests, coords_tests
 
 
 def next_coords(coords: list, path):
+    coords = coords.copy()
     direction, distance = path[0], int(path[1:])
 
     # x-axis / y-axis
@@ -9,32 +10,79 @@ def next_coords(coords: list, path):
     # positive / negative axis movement
     multiplier = 1 if direction in {"U", "R"} else -1
 
-    coords[change_pos] = distance * multiplier
+    coords[change_pos] = coords[change_pos] + (distance * multiplier)
     return coords
 
 
-def check_for_intersection(l_coord_pair, s_coord_pair):
-    pass
+def extract_coords_from_path(x1, y1, x2, y2):
+    if x1 == x2:
+        r_start = min(y1, y2)
+        r_stop = max(y1, y2) + 1
+        update_pos = 1
+        static_pos = 0
+        static_val = x1
+    else:
+        r_start = min(x1, x2)
+        r_stop = max(x1, x2) + 1
+        update_pos = 0
+        static_pos = 1
+        static_val = y1
+
+    path_coords = []
+    for c in range(r_start, r_stop):
+        new_path = [None, None]
+        new_path[update_pos] = c
+        new_path[static_pos] = static_val
+        path_coords.append(new_path)
+
+    return path_coords
+
+
+def check_for_intersection(coords_a, coords_b):
+    coords = [None, None]
+    exists = False
+
+    for coord_pair in coords_a:
+        if coord_pair in coords_b and coord_pair != [0, 0]:
+            exists = True
+            coords = coord_pair
+            break
+
+    return exists, coords
 
 
 def traverse_wires(wire_a, wire_b):
     # longest wire, shortest wire
     a_long = len(wire_a) >= len(wire_b)
-    l_wire, s_wire = wire_a, wire_b if a_long else wire_b, wire_a
+    l_wire, s_wire = (wire_a, wire_b) if a_long else (wire_b, wire_a)
 
     # long wire coords 0 & 1, short wire coords 0 & 1
     lc0 = [0, 0]
     lc1 = [0, 0]
-    sc0 = [0, 0]
-    sc1 = [0, 0]
+    intersections = []
 
     for l_path in l_wire:
         lc0 = lc1
         lc1 = next_coords(lc0, l_path)
+        lc_coords = extract_coords_from_path(*lc0, *lc1)
+
+        # had to move these here because they never reset...
+        sc0 = [0, 0]
+        sc1 = [0, 0]
 
         for s_path in s_wire:
             sc0 = sc1
             sc1 = next_coords(sc0, s_path)
+            sc_coords = extract_coords_from_path(*sc0, *sc1)
+
+            exists, coords = check_for_intersection(lc_coords, sc_coords)
+            if exists:
+                intersections.append(tuple(coords))
+
+    reduced_intersections = [
+        sum([abs(c1), abs(c2)]) for c1, c2 in intersections
+    ]
+    return min(reduced_intersections)
 
 
 def test_next_coords():
@@ -43,11 +91,25 @@ def test_next_coords():
         assert next_coords(origin, path) == answer
 
 
+def test_extract_coords_from_path():
+    tests = [
+        ([0, 0, 0, 2], [[0, 0], [0, 1], [0, 2]]),
+        ([0, 0, 0, 3], [[0, 0], [0, 1], [0, 2], [0, 3]]),
+        ([1, 0, 1, 3], [[1, 0], [1, 1], [1, 2], [1, 3]]),
+    ]
+    for test, answer in tests:
+        assert extract_coords_from_path(*test) == answer
+
+
 def test_traverse_wires():
     for test, answer in traverse_tests:
         wire_a, wire_b = test
-        assert traverse_wires(*test) == answer
+        # print(traverse_wires(wire_a, wire_b), answer)
+        assert traverse_wires(wire_a, wire_b) == answer
 
 
 test_next_coords()
+test_extract_coords_from_path()
+test_traverse_wires()
 wire_a, wire_b = wires
+print(traverse_wires(wire_a, wire_b))
