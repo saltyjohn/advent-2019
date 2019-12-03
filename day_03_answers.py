c2024ea0dbd54d4f0ec401841b1dcc166a3ca7d4
John Salzman <john.r.salzman@gmail.com>
2019-12-03 17:00:52 -0500

    part complete and much faster than the first try, part 2 is coming along

--- day_03_answers.py
+++ day_03_answers.py
@@ -1,9 +1,44 @@
-from day_03_inputs import wires, traverse_tests, coords_tests
+from day_03_inputs import wires, traverse_tests
+from pprint import pprint
 
 
-def next_coords(coords: list, path):
+def traverse_wires(wire_a, wire_b, return_manhattan=True):
+    # extract paths from wire
+    wire_a_path = extract_path_from_wire(wire_a)
+    wire_b_path = extract_path_from_wire(wire_b)
+
+    intersections = []
+    detailed_intersections = []
+
+    # check each line segment from wire_a with all segments from wire_b
+    for idx_a in range(0, len(wire_a_path) - 1):
+        wire_a_line = [wire_a_path[idx_a], wire_a_path[idx_a + 1]]
+        for idx_b in range(0, len(wire_b_path) - 1):
+            wire_b_line = [wire_b_path[idx_b], wire_b_path[idx_b + 1]]
+
+            exists, coords = check_for_intersection(wire_a_line, wire_b_line)
+            if exists:
+                intersections.append(tuple(coords))
+                detailed_intersections.append([(idx_a + 1, idx_b + 1), coords])
+
+    if return_manhattan:
+        return min([sum([abs(x), abs(y)]) for x, y in intersections])
+    else:
+        print(detailed_intersections)
+
+
+def extract_path_from_wire(wire, origin=[0, 0]):
+    path = [origin]
+    for direction in wire:
+        new_coords = get_next_coords(path[-1], direction)
+        path.append(new_coords)
+
+    return path
+
+
+def get_next_coords(coords: list, direction):
     coords = coords.copy()
-    direction, distance = path[0], int(path[1:])
+    direction, distance = direction[0], int(direction[1:])
 
     # x-axis / y-axis
     change_pos = 0 if direction in {"R", "L"} else 1
@@ -14,102 +49,55 @@ def next_coords(coords: list, path):
     return coords
 
 
-def extract_coords_from_path(x1, y1, x2, y2):
-    if x1 == x2:
-        r_start = min(y1, y2)
-        r_stop = max(y1, y2) + 1
-        update_pos = 1
-        static_pos = 0
-        static_val = x1
-    else:
-        r_start = min(x1, x2)
-        r_stop = max(x1, x2) + 1
-        update_pos = 0
-        static_pos = 1
-        static_val = y1
-
-    path_coords = []
-    for c in range(r_start, r_stop):
-        new_path = [None, None]
-        new_path[update_pos] = c
-        new_path[static_pos] = static_val
-        path_coords.append(new_path)
-
-    return path_coords
-
-
-def check_for_intersection(coords_a, coords_b):
-    coords = [None, None]
+def check_for_intersection(line_a, line_b):
     exists = False
+    coords = [None, None]
 
-    for coord_pair in coords_a:
-        if coord_pair in coords_b and coord_pair != [0, 0]:
-            exists = True
-            coords = coord_pair
-            break
+    sp_a, ra_start, ra_end = static_pos_and_range(*line_a)
+    sp_b, rb_start, rb_end = static_pos_and_range(*line_b)
+
+    # if both static positions are equal, they are parallel / no intersection
+    if sp_a == sp_b:
+        return exists, coords
+
+    static_val_a = line_a[0][sp_a]
+    static_val_b = line_b[0][sp_b]
+
+    # check if static values are within of other's range
+    a_in_b_range = rb_start <= static_val_a <= rb_end
+    b_in_a_range = ra_start <= static_val_b <= ra_end
+
+    # if both are true, the line intersects at the two static values
+    exists = a_in_b_range and b_in_a_range
+    coords[sp_a] = static_val_a
+    coords[sp_b] = static_val_b
+
+    # the origin does not count as and intersection
+    if coords == [0, 0]:
+        exists = False
 
     return exists, coords
 
 
-def traverse_wires(wire_a, wire_b):
-    # longest wire, shortest wire
-    a_long = len(wire_a) >= len(wire_b)
-    l_wire, s_wire = (wire_a, wire_b) if a_long else (wire_b, wire_a)
+def static_pos_and_range(coord_a, coord_b):
+    if coord_a[0] == coord_b[0]:
+        static_pos, r_pos = 0, 1
+    elif coord_a[1] == coord_b[1]:
+        static_pos, r_pos = 1, 0
 
-    # long wire coords 0 & 1, short wire coords 0 & 1
-    lc0 = [0, 0]
-    lc1 = [0, 0]
-    intersections = []
-
-    for l_path in l_wire:
-        lc0 = lc1
-        lc1 = next_coords(lc0, l_path)
-        lc_coords = extract_coords_from_path(*lc0, *lc1)
-
-        # had to move these here because they never reset...
-        sc0 = [0, 0]
-        sc1 = [0, 0]
-
-        for s_path in s_wire:
-            sc0 = sc1
-            sc1 = next_coords(sc0, s_path)
-            sc_coords = extract_coords_from_path(*sc0, *sc1)
-
-            exists, coords = check_for_intersection(lc_coords, sc_coords)
-            if exists:
-                intersections.append(tuple(coords))
-
-    reduced_intersections = [
-        sum([abs(c1), abs(c2)]) for c1, c2 in intersections
-    ]
-    return min(reduced_intersections)
-
-
-def test_next_coords():
-    for test, answer in coords_tests:
-        origin, path = test
-        assert next_coords(origin, path) == answer
-
-
-def test_extract_coords_from_path():
-    tests = [
-        ([0, 0, 0, 2], [[0, 0], [0, 1], [0, 2]]),
-        ([0, 0, 0, 3], [[0, 0], [0, 1], [0, 2], [0, 3]]),
-        ([1, 0, 1, 3], [[1, 0], [1, 1], [1, 2], [1, 3]]),
-    ]
-    for test, answer in tests:
-        assert extract_coords_from_path(*test) == answer
+    r = [coord_a[r_pos], coord_b[r_pos]]
+    r_start = min(r)
+    r_end = max(r)
+    return static_pos, r_start, r_end
 
 
 def test_traverse_wires():
     for test, answer in traverse_tests:
         wire_a, wire_b = test
-        # print(traverse_wires(wire_a, wire_b), answer)
         assert traverse_wires(wire_a, wire_b) == answer
 
 
-test_next_coords()
-test_extract_coords_from_path()
 test_traverse_wires()
 wire_a, wire_b = wires
-print(traverse_wires(wire_a, wire_b))
+print(traverse_wires(wire_a, wire_b))  # 2180
+# pprint(traverse_wires(wire_a, wire_b, return_manhattan=False))
