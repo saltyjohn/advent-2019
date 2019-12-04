aa02bb9a1e42ac6b28f9588b1ec00369d97c5f21
John Salzman <john.r.salzman@gmail.com>
2019-12-04 02:46:08 -0500

    working on part 2, but it's a doozie

--- day_04_answers.py
+++ day_04_answers.py
@@ -1,43 +1,47 @@
-def is_inc_with_exception_of_adj_euqal_or_equilvalence(num):
+def check_has_equal(num):
     string = str(num)
-    is_inc = True
+    has_equal = False
     for i in range(0, len(string) - 1):
-        a = string[i]
-        b = string[i + 1]
-        if a < b and check_has_only_one_equal_adj(string):
-            is_inc = False
+        val_1, val_2 = int(string[i]), int(string[i + 1])
+        if val_1 == val_2:
+            has_equal = True
+            break
+    return has_equal
 
-    return is_inc
 
-
-def check_has_only_one_equal_adj(num):
+def check_always_incs_or_equals(num):
     string = str(num)
-    found = False
+    always_inc = True
     for i in range(0, len(string) - 1):
-        s_val_1 = string[i]
-        s_val_2 = string[i + 1]
-        vals_is_equal = s_val_1 == s_val_2
-        if not found and vals_is_equal:
-            found = True
-        elif found and vals_is_equal:
-            return False
-    return found
+        val_1, val_2 = int(string[i]), int(string[i + 1])
+        if val_1 > val_2:
+            always_inc = False
+            break
+    return always_inc
 
 
-def test():
-    tests = [(111111, True), (223450, False), (123789, False)]
-    for test, answer in tests:
-        is_inc = is_inc_with_exception_of_adj_euqal_or_equilvalence(test)
-        one_equal = check_has_only_one_equal_adj(test)
-        assert (is_inc and one_equal) == answer
-
-
-def main():
+def part_1():
+    answers = []
     for i in range(264360, 746325 + 1):
-        a = check_has_only_one_equal_adj(i)
-        b = is_inc_with_exception_of_adj_euqal_or_equilvalence(i)
-        if a and b:
-            return i
+        len_is_good = len(str(i)) == 6
+        equal = check_has_equal(i)
+        inc = check_always_incs_or_equals(i)
+        if len_is_good and inc and equal:
+            answers.append(i)
+    return len(answers)
 
 
-main()
+def test_1():
+    tests = [(111111, True), (223450, False), (123789, False)]
+    for test_num, answer in tests:
+        inc = check_always_incs_or_equals(test_num)
+        equal = check_has_equal(test_num)
+        assert (inc and equal) == answer
+
+
+def test_2():
+    tests = [(112233, True), (123444, False), (111122, True)]
+
+
+test_1()
+print(part_1())  # 945
